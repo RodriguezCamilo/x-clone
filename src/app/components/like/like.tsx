488cf744f47b473e403f050fc857ce935695b82f
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2024-09-06 06:52:13 -0300

    arregle links de post, visibilidad de este y detalles al boton de like

--- src/app/components/like/like.tsx
+++ src/app/components/like/like.tsx
@@ -22,8 +22,9 @@ export default function LikeButton({ post_id }: any) {
                 await handleLike({ post_id })
                 setLike(!like)
             }}
+            className='rounded-full size-8 flex items-center justify-center hover:bg-pink-600/20 transition group'
         >
-            {like ? <IconHeartFilled className='size-5 text-pink-500' /> : <IconHeart className="size-5 text-white/50" />}
+            {like ? <IconHeartFilled className='size-5 text-pink-600' /> : <IconHeart className="size-5 text-white/50 group-hover:text-pink-600 transition" />}
         </button>
     )
 }
--- src/app/components/posts/post-card.tsx
+++ src/app/components/posts/post-card.tsx
@@ -16,33 +16,33 @@ export function PostCard({
     fullName: string,
     content: string,
     id: string,
-})
-
-{
+}) {
     return (
-        <article className="text-left flex flex-row w-full p-4 border-b-2 border-zinc-700 gap-2 bg-gray/0 transition hover:bg-zinc-300/5 cursor-pointer relative">
-            <Link className="absolute inset-0 -z-10" href={`/posts/${id}`}></Link>
-                <Link href={`/perfil/${userName}`} className="flex flex-row">
+        <article className="text-left flex flex-row w-full p-4 pb-2 border-b-2 border-zinc-700 gap-2 bg-gray/0 transition hover:bg-zinc-300/5 cursor-pointer relative">
+            <Link className="absolute inset-0 z-0 " aria-hidden="true" href={`/posts/${id}`}></Link>
+            <Link href={`/perfil/${userName}`} className="flex flex-row z-10">
                 {avatarUrl != 'Unknown' ? <img src={avatarUrl} className="rounded-full size-10" alt="" /> : <div className='rounded-full bg-zinc-500/50 size-10 flex items-center justify-center'><IconUser /></div>}
-                </Link>
-                <main className="flex flex-col">
-                    <div className="flex flex-row gap-2">
-                        <Link href={`/perfil/${userName}`} className="font-bold hover:underline">{fullName}</Link>
-                        <p className="font-light text-white/50">@{userName}</p>
-                    </div>
-                    <p>
-                        {content}
-                    </p>
-                    <footer className="flex flex-row justify-between pt-2">
-                        <button>
-                            <IconMessageCircle className="size-5 text-white/50" />
-                        </button>
-                        <button>
-                            <IconRepeat className="size-5 text-white/50" />
-                        </button>
+            </Link>
+            <main className="flex flex-col w-full">
+                <div className="flex flex-row gap-2">
+                    <Link href={`/perfil/${userName}`} className="font-bold hover:underline z-10">{fullName}</Link>
+                    <Link href={`/perfil/${userName}`} className="font-light text-white/50">@{userName}</Link>
+                </div>
+                <p>
+                    {content}
+                </p>
+                <footer className="flex flex-row w-full justify-between pt-2">
+                    <button>
+                        <IconMessageCircle className="size-5 text-white/50" />
+                    </button>
+                    <button>
+                        <IconRepeat className="size-5 text-white/50" />
+                    </button>
+                    <div className='z-10'>
                         <LikeButton post_id={id} />
-                    </footer>
-                </main>
+                    </div>
+                </footer>
+            </main>
         </article >
     )
 }
