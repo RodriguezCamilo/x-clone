898bd7d419bf2dacad947afc678e1b420822718d
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2024-09-24 17:32:42 -0300

    sidebar actualizada

--- src/app/components/sidebar/sidebar.tsx
+++ src/app/components/sidebar/sidebar.tsx
@@ -1,9 +1,11 @@
+import Project from "./project"
 import WhoFollow from "./who-follow"
 
 export default function SideBar() {
   return (
-    <div className="flex flex-col w-full xl:w-2/3 p-4">
+    <div className="flex flex-col w-full xl:w-2/3 p-4 gap-4">
       <WhoFollow/>
+      <Project/>
     </div>
   )
 }
