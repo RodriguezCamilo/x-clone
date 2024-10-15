4da34d631a110e9468f179fea94f60a6c7eea31d
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2024-10-14 23:39:32 -0300

    users aleatorios en sidebar

--- src/app/components/sidebar/who-follow.tsx
+++ src/app/components/sidebar/who-follow.tsx
@@ -3,14 +3,12 @@ import Link from "next/link";
 import { IconUser } from "@tabler/icons-react";
 import FollowButton from "../follows/follow-button";
 import { fetchFollowStatus } from "@/app/actions/follow-action";
+import { Users } from "@/app/types/users";
 
 export default async function WhoFollow() {
   const supabase = createClient();
 
-  const { data: users, error: userError } = await supabase
-    .from("users")
-    .select("id, name, user_name, avatar_url, created_at, bio")
-    .limit(3);
+  let { data: users, error: userError } = await supabase.rpc("get_random_user");
 
   if (userError) {
     console.error(userError);
@@ -18,7 +16,7 @@ export default async function WhoFollow() {
   }
 
   const followStatuses = await Promise.all(
-    users?.map(async (user) => ({
+    users?.map(async (user: Users) => ({
       ...user,
       isFollowing: await fetchFollowStatus(user.id),
     })) || []
@@ -33,7 +31,10 @@ export default async function WhoFollow() {
             key={user.id}
             className="flex flex-row items-center space-x-4 hover:bg-white/5 transition z-0 justify-between relative"
           >
-            <Link href={`/perfil/${user.user_name}`} className="absolute inset-0 z-0"></Link>
+            <Link
+              href={`/perfil/${user.user_name}`}
+              className="absolute inset-0 z-0"
+            ></Link>
             <div className="flex flex-row gap-2 items-center">
               <div className="">
                 {user.avatar_url ? (
@@ -63,7 +64,12 @@ export default async function WhoFollow() {
           </div>
         ))}
       </div>
-      <Link href={'/conectar'} className="w-full hover:bg-white/5 px-4 py-3 text-sky-600">Mostrar más</Link>
+      <Link
+        href={"/conectar"}
+        className="w-full hover:bg-white/5 px-4 py-3 text-sky-600"
+      >
+        Mostrar más
+      </Link>
     </div>
   );
 }
