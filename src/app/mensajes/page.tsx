47bc7fb0ee35e725b974c5ba5e154edd0315e393
Camilo Rodriguez <camiloceatrabajo@gmail.com>
2024-10-15 07:29:15 -0300

    borre inecesario de page de mensajes

--- src/app/mensajes/page.tsx
+++ src/app/mensajes/page.tsx
@@ -1,14 +1,9 @@
 "use server"
 
-import { getConversations } from "../actions/chat-action";
 import MenssagesContainer from "../components/messages/messages-container";
 import NavBar from "../components/navbar/navbar";
-import { DataUser } from "../utils/supabase/user";
 
 export default async function Mensajes() {
-  const user = await DataUser();
-  const conversations = await getConversations(user.user?.id);
-  
 
   return (
     <body className="min-h-screen w-full flex flex-col-reverse md:flex-row text-white bg-black">
